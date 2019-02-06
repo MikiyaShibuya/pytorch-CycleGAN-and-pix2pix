655515d117f49e1df7eb5cec796c429f4d23f44a
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2019-02-06 21:01:56 +0900

    My data augmentation due to use fir thermal data(0 to 10000 of range).

--- data/paired_dataset.py
+++ data/paired_dataset.py
@@ -4,6 +4,8 @@ from data.base_dataset import BaseDataset, get_params, get_transform
 import torchvision.transforms as transforms
 from data.image_folder import make_paired_dataset
 from PIL import Image
+import numpy as np
+from data.transformation import Compose, RandomHorizontalFlip, Scale, RandomCrop
 
 
 class PairedDataset(BaseDataset):
@@ -19,11 +21,6 @@ class PairedDataset(BaseDataset):
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
 
-        #TODO これなに？
-        #self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
-        #self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
-
-
 
         BaseDataset.__init__(self, opt)
         self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
@@ -36,12 +33,15 @@ class PairedDataset(BaseDataset):
         btoA = self.opt.direction == 'BtoA'
         self.input_nc = self.opt.output_nc if btoA else self.opt.input_nc       # get the number of channels of input image
         self.output_nc = self.opt.input_nc if btoA else self.opt.output_nc      # get the number of channels of output image
-        self.transform_A = get_transform(self.opt, grayscale=(self.input_nc == 1))
-        self.transform_B = get_transform(self.opt, grayscale=(self.output_nc == 1))
+
+        self.transforms = Compose([
+            RandomHorizontalFlip(),
+            RandomCrop(opt.load_size),
+            Scale(opt.crop_size)
+        ])
 
 
-
-    def __getitem__(self, index):
+    def __oldgetitem__(self, index):
         """Return a data point and its metadata information.
 
         Parameters:
@@ -62,11 +62,37 @@ class PairedDataset(BaseDataset):
 
         # apply the same transform to both A and B
         transform_params = get_params(self.opt, A.size)
+        transform_params = get_params(self.opt, A.size)
         A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
         B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
 
         A = A_transform(A)
         B = B_transform(B)
+
+        return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
+
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index - - a random integer for data indexing
+
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor) - - an image in the input domain
+            B (tensor) - - its corresponding image in the target domain
+            A_paths (str) - - image paths
+            B_paths (str) - - image paths (same as A_paths)
+        """
+        # read a image given a random integer index
+        A_path = self.A_paths[index]
+        B_path = self.B_paths[index]
+        A = np.asarray(Image.open(A_path)).astype(np.float32)
+        B = np.asarray(Image.open(B_path)).astype(np.float32)
+        # split AB image into A and B
+
+        # apply the same transform to both A and B
+        A, B = self.transforms(A, B)
 
         return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
 
