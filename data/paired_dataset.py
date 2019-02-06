2bb83ffb12f66d2f99ff78c9978d6ce3d8c9283f
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2019-02-06 17:39:11 +0900

    Add dataset handles different channels paire images.

---
+++ data/paired_dataset.py
@@ -0,0 +1,75 @@
+import os.path
+import random
+from data.base_dataset import BaseDataset, get_params, get_transform
+import torchvision.transforms as transforms
+from data.image_folder import make_paired_dataset
+from PIL import Image
+
+
+class PairedDataset(BaseDataset):
+    """A dataset class for paired image dataset.
+
+    The paired images are saved as same name in trainA, trainB.
+    """
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+
+        #TODO これなに？
+        #self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
+        #self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
+
+
+
+        BaseDataset.__init__(self, opt)
+        self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
+        self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')  # create a path '/path/to/data/trainB'
+
+        self.A_paths, self.B_paths = make_paired_dataset(self.dir_A, self.dir_B, opt.max_dataset_size)
+
+        self.A_size = len(self.A_paths)  # get the size of dataset A
+        self.B_size = len(self.B_paths)  # get the size of dataset B
+        btoA = self.opt.direction == 'BtoA'
+        self.input_nc = self.opt.output_nc if btoA else self.opt.input_nc       # get the number of channels of input image
+        self.output_nc = self.opt.input_nc if btoA else self.opt.output_nc      # get the number of channels of output image
+        self.transform_A = get_transform(self.opt, grayscale=(self.input_nc == 1))
+        self.transform_B = get_transform(self.opt, grayscale=(self.output_nc == 1))
+
+
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index - - a random integer for data indexing
+
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor) - - an image in the input domain
+            B (tensor) - - its corresponding image in the target domain
+            A_paths (str) - - image paths
+            B_paths (str) - - image paths (same as A_paths)
+        """
+        # read a image given a random integer index
+        A_path = self.A_paths[index]
+        B_path = self.B_paths[index]
+        A = Image.open(A_path)
+        B = Image.open(B_path).convert('RGB')
+        # split AB image into A and B
+
+        # apply the same transform to both A and B
+        transform_params = get_params(self.opt, A.size)
+        A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
+        B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
+
+        A = A_transform(A)
+        B = B_transform(B)
+
+        return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
+
+    def __len__(self):
+        """Return the total number of images in the dataset."""
+        return len(self.A_paths)
