84d2f98a4d5b60ded3de5084e87974d24ebe1787
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2019-02-06 21:00:24 +0900

    Transform randomly to data augmentation.

---
+++ data/transformation.py
@@ -0,0 +1,42 @@
+import numpy as np
+from skimage import transform
+import torch
+
+class Compose(object):
+    def __init__(self, transformations):
+        self.transformations = transformations
+
+    def __call__(self, fir_image, rgb_image):
+        for t in self.transformations:
+            fir_image, rgb_image = t(fir_image, rgb_image)
+
+        fir_image = fir_image.reshape(1, fir_image.shape[0], fir_image.shape[1])
+        fir_tensor = torch.tensor(fir_image)
+        rgb_tensor = torch.tensor(rgb_image.transpose(2,0,1))
+        return fir_tensor, rgb_tensor
+
+class RandomHorizontalFlip(object):
+    def __call__(self, fir_image, rgb_image):
+        if np.random.rand() < 0.5:
+            return np.fliplr(fir_image), np.fliplr(rgb_image)
+        else:
+            return fir_image, rgb_image
+
+class RandomCrop(object):
+    # crop_size以上のランダムなサイズで正方形クロップを行う
+    def __init__(self, crop_size):
+        self.min_crop_size = crop_size
+    def __call__(self, fir_image, rgb_image):
+        max_crop_size = np.min(fir_image.shape)
+        crop_size = int(np.random.uniform(self.min_crop_size, max_crop_size))
+        x = np.random.randint(fir_image.shape[1]-crop_size+1)
+        y = np.random.randint(fir_image.shape[0]-crop_size+1)
+        return fir_image[y:y+crop_size, x:x+crop_size], rgb_image[y:y+crop_size, x:x+crop_size, :]
+
+class Scale(object):
+    def __init__(self, size):
+        self.size = size
+    def __call__(self, fir, rgb):
+        fir_image = transform.resize(fir, (self.size, self.size), mode='reflect', anti_aliasing=True).astype(np.float32)
+        rgb_image = transform.resize(rgb, (self.size, self.size), mode='reflect', anti_aliasing=True).astype(np.float32)
+        return fir_image, rgb_image
